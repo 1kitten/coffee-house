63e9cb6606b547da8e811c1eb53811b51744ce7d
ShineWxrk <ShineWxrk@gmail.com>
2023-04-10 20:38:54 +0300

    update animation card

--- frontend/coffee-house/src/components/Hero/Hero.jsx
+++ frontend/coffee-house/src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import styles from './Hero.module.scss';
 import hero_img from '../../assets/png/hero.png'
+import { forwardRef, useEffect } from 'react'
 
 export const Hero = ({ }) => {
   return(
@@ -11,8 +12,8 @@ export const Hero = ({ }) => {
       <div className={styles.container} >
         <h1 className={styles.title}>Coffee House</h1>
         <p className={styles.description}>Today's good mood is sponsored by coffee search for your coffee now</p>
-        <btn href='#!' className={styles.btn}>Shop now</btn>
-        <btn href='#!' className={styles.btn}>Catalog</btn>
+        <a href='#!' className={styles.btn}>Shop now</a>
+        <a href='#!' className={styles.btn}>Catalog</a>
       </div>
     </div>
   </section>
