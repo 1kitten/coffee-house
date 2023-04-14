54064c191dc1a60de54b6250ce4c59ff93da9255
ShineWxrk <ShineWxrk@gmail.com>
2023-04-15 01:48:41 +0300

    minor fix

--- frontend/coffee-house/src/App.jsx
+++ frontend/coffee-house/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.scss';
-import { useState, useEffect,  } from 'react'
+import { useState, useEffect, Suspense } from 'react'
 import { Header } from './components/Header/Header'
 import { Hero } from './components/Hero/Hero'
 import { Story } from './components/Story/Story'
@@ -7,18 +7,12 @@ import { Services } from './components/Services/Services';
 import { HeroCard } from './components/HeroCard/HeroCard';
 import { Footer } from './components/Footer/Footer';
 
+
 const BASE_URL = "http://127.0.0.1:8000/"
 
 function App() {
-  const vh = window.innerHeight / 100;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-
   const [goodbye, setGoodbye] = useState()
   const [scroll, setScroll] = useState(window.pageYOffset)
-
-  useEffect(() => {
-    fetchGoodbye()
-  }, [])
 
   const fetchGoodbye = async () => {
     const url = BASE_URL
@@ -32,7 +26,7 @@ function App() {
 
   return (
     <>
-      <div onWheel={check} className="app">
+     <div onWheel={check} className="app">
         <Header />
         <Hero />
         <Story />
