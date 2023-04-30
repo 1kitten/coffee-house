6efcb680e8b268284af80c4a7630c379565e6d49
ShineWxrk <ShineWxrk@gmail.com>
2023-04-30 21:09:18 +0300

    add route

---
+++ frontend/coffee-house/src/components/HeroPage/HeroPage.jsx
@@ -0,0 +1,33 @@
+import styles from './HeroPage.module.scss';
+import { useState } from 'react'
+
+import { Header } from '../Header/Header'
+import { Hero } from '../Hero/Hero'
+import { Story } from '../Story/Story'
+import { Services } from '../Services/Services';
+import { HeroCard } from '../HeroCard/HeroCard';
+import { Footer } from '../Footer/Footer';
+import { Offer } from '../Offer/Offer';
+import { Reserve } from '../Reserve/Reserve';
+import { Recomended } from '../Recomended/Recomended';
+
+export const HeroPage = ({ }) => {
+
+  const [scroll, setScroll] = useState(window.pageYOffset)
+
+  const check = () => {
+    setScroll(window.pageYOffset)
+  }
+
+  return (
+  <div onWheel={check} className={styles.heroPage}>
+    <Hero />
+    <Story />
+    <Services />
+    <Offer />
+    <Reserve />
+    <Recomended />
+    <HeroCard scroll={scroll}/>
+  </div>
+  )
+}
