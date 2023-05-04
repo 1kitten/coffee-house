1aa5e58754f8c157c13b5a43bb5c7035f63271af
ShineWxrk <ShineWxrk@gmail.com>
2023-05-04 20:21:23 +0300

    add categories

--- frontend/coffee-house/src/components/ServicesPage/ServicesPage.jsx
+++ frontend/coffee-house/src/components/ServicesPage/ServicesPage.jsx
@@ -1,8 +1,30 @@
 import styles from './ServicesPage.module.scss';
+import { Carousel } from 'react-carousel3';
 
+export const ServicesPage = ({ }) => {
+  let radius = window.innerWidth
+    if (window.innerWidth > 1000) {
+      radius = 500
+    } else if (1000 > window.innerWidth  && window.innerWidth > 600) {
+      radius = 300
+    } else {
+      radius = 100
+    }
 
-export const ServicesPage = ({ }) => (
-  <div className={styles.servicesPage}>
-    ServicesPage Component
-  </div>
-);
+  const checkClick = (e) => {
+    console.log(e.target.dataset.action)
+  }
+  
+  return(
+  <section className={styles.servicesPage}>
+    <div onClick={checkClick} className={styles.wrapper}>
+      <Carousel height={200} xRadius={radius}>
+        <label key={1} onClick={checkClick} className={styles.title} data-action="Hleb">Hleb</label>
+        <label key={2} onClick={checkClick} className={styles.title} data-action="Stakani">Stakani</label>
+        <label key={3} onClick={checkClick} className={styles.title} data-action="Popit">Popit'</label>
+        <label key={4} onClick={checkClick} className={styles.title} data-action="Zerna">Zerna</label>
+      </Carousel>
+    </div>
+  </section>
+  )
+};
