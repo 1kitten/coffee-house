8fd3143a778dc35cfe02d549c054604a4f8a607e
ShineWxrk <ShineWxrk@gmail.com>
2023-05-06 23:12:34 +0300

    add items

--- frontend/coffee-house/src/components/Item/Item.jsx
+++ frontend/coffee-house/src/components/Item/Item.jsx
@@ -1,8 +1,36 @@
 import styles from './Item.module.scss';
+import coffee from '../../assets/png/coffee.png'
+import pastry from '../../assets/png/pastry.png'
 
+export const Item = ({ type }) => {
+  let img
+  let title
+  switch(type) {
+    case 'pastry':  
+      img = pastry
+      title = 'bulki'
+      break
+  
+    case 'coffee':
+      img = coffee
+      title = 'Coffee'
+      break
 
-export const Item = ({ }) => (
+    case 'way':  
+      img = pastry
+      title = 'bulki'
+      break
+  
+    case 'beans':
+      img = coffee
+      title = 'Coffee'
+      break
+  }
+  
+  return(
   <div className={styles.item}>
-    Item Component
+    <img className={styles.img} src={img} />
+    <label  className={styles.title}>{title}</label>
   </div>
-);
+  )
+};
--- frontend/coffee-house/src/index.js
+++ frontend/coffee-house/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index2.scss';
+import './index.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
