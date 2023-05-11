fa0991e8e1c64e0417c957558ad9ee50c1b940d1
ShineWxrk <ShineWxrk@gmail.com>
2023-05-11 23:37:39 +0300

    fix modal

--- frontend/coffee-house/src/components/Modal/Modal.jsx
+++ frontend/coffee-house/src/components/Modal/Modal.jsx
@@ -4,9 +4,9 @@ import styles from './Modal.module.scss';
 export const Modal = ({ active, setActive, data }) => (
   <div className={active ? styles.active : styles.modal} onClick={() => setActive(false)}>
     <div className={styles.content} onClick={(e) => e.stopPropagation()}>
-      <h1>{data.title}</h1>
+      <h2 className={styles.item_title}>{data.title}</h2>
       <p className={styles.description}>{data.content}</p>
-      <img src={data.img} className={styles.description} />
+      <img src={data.img} className={styles.img} />
     </div>
   </div>
 );
