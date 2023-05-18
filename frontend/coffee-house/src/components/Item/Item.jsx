5713d35366b3f8dea79a6ad9dd24e8f7f3b0029f
ShineWxrk <ShineWxrk@gmail.com>
2023-05-18 22:40:57 +0300

    add item styles

--- frontend/coffee-house/src/components/Item/Item.jsx
+++ frontend/coffee-house/src/components/Item/Item.jsx
@@ -12,38 +12,44 @@ export const Item = ({ type }) => {
   let img
   let title
   let scane
+  let price
   
   switch(type) {
     case 'pastry':  
       img = pastry_1
       title = 'bulki'
       scane = 'https://prod.spline.design/pc6odhdUpSVcke4G/scene.splinecode'
+      price = 20.00
       break
   
     case 'coffee':
       img = coffee_1
       title = 'Coffee'
       scane = 'https://prod.spline.design/F4x-qvtw6z9uSYOo/scene.splinecode'
+      price = 5.00
       break
 
     case 'way':  
       img = coffee_2
       title = 'bulki'
       scane = 'https://prod.spline.design/Dj2et5rdCMsWrGd4/scene.splinecode'
+      price = 15.00
       break
   
     case 'beans':
       img = coffee_3
       title = 'Coffee'
       scane = 'https://prod.spline.design/Dj2et5rdCMsWrGd4/scene.splinecode'
+      price = 10.00
       break
   }
   
   return(
-  <Link to={'/info'} state={{ scane }}>
+  <Link to={'/info'} state={{ scane, title, price }}>
     <div className={styles.item}>
       <img className={styles.img} src={img} />
-      <label  className={styles.title}>{title}</label>
+      <label className={styles.title}>{title}</label>
+      <label className={styles.title}>{price} $</label>
     </div>
   </Link>
   )
