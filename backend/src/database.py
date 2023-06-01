f281de0f0637f563bba42ead14263472f9860043
Maksim Strelchenko <maksim.strelchenko99@gmail.com>
2023-06-01 19:07:24 +0300

    Change FastAPI to Flask

--- backend/src/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-engine = create_engine(
-    "sqlite:///coffee_db.db"
-)
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,23 +1,8 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from database import session
+from flask import Flask
+
+app = Flask(__name__)
 
 
-app = FastAPI(
-    title='coffee-shop'
-)
-
-origins = ["http://localhost:3000/"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get('/api/v1/items/{category}')
-def get_item_by_category(category: str):
-    ...
+@app.route('/', methods=['GET'])
+def index():
+    return {"hello": "world"}
